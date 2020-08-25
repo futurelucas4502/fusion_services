e43a856d295bea9172481123b1e9ec82f699a186
Lucas Wilson <lucaswilson4502@outlook.com>
2020-08-26 00:12:29 +0100

    Splash screen & init setup

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+function createWindow() {
+  console.log("Ready to create windows...")
+
+  // create a new `splash`-Window 
+  const splash = new BrowserWindow({
+    width: 500,
+    height: 300,
+    frame: false,
+    alwaysOnTop: true,
+    show: false
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  splash.loadFile("splash.html")
+
+  let mainWindow
+
+  splash.once('ready-to-show', () => {
+    console.log("Splash ready...")
+    splash.show()
+    // and load the index.html of the app.
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      },
+      show: false
+    })
+    mainWindow.loadFile("views/index.html")
+
+    // if main window is ready to show, then destroy the splash window and show up the main window
+    mainWindow.once('ready-to-show', () => {
+      console.log("Main ready. Waiting 3 seconds for splash...")
+      setTimeout(() => {
+        console.log("3 Seconds over killing splash and loading main...")
+        splash.destroy();
+        mainWindow.show();
+      }, 3000);
+    });
+  });
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -24,7 +55,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -36,7 +67,10 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    db.close()
+    app.quit()
+  }
 })
 
 // In this file you can include the rest of your app's specific main process
